48fd908363a86e981e9f352894db7f644117827f
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-09 16:33:48 +0100

    Detect lines in the image using Hough transform

--- scancube.py
+++ scancube.py
@@ -1,15 +1,22 @@
 import cv2
+import numpy as np
 
 
-def scan_cube(cubeImage, edgeImageFile=None):
+def scan_cube(cubeImage, edgeImageFile=None, linesImageFile=None):
     """Scan the given cube image and return the colours of the cube face.
 
     Returns None if cube face could not be scanned.
     Saves the intermediate edge image generated to the filename given.
+    Saves the intermediate lines image generated to the filename given.
     """
     edgeImage = _detect_edges(cubeImage)
     if edgeImageFile is not None:
         cv2.imwrite(edgeImageFile, edgeImage)
+
+    lines = _detect_lines(edgeImage)
+    if linesImageFile is not None:
+        linesImage = _draw_lines(cubeImage, lines)
+        cv2.imwrite(linesImageFile, linesImage)
 
     return None
 
@@ -21,3 +28,31 @@ def _detect_edges(image):
     edgeImage = cv2.Canny(blurredGrayImage, 0, 50)
 
     return edgeImage
+
+
+def _detect_lines(edgeImage, threshold=125):
+    """Detects lines in the given edge image using the Hough transform."""
+    lines = cv2.HoughLines(edgeImage, 1, np.pi/180, threshold)
+    # Change line representation from [(rho, theta)] to just (rho, theta)
+    lines = [line[0] for line in lines]
+
+    return lines
+
+
+def _draw_lines(image, lines):
+    """Returns a copy of the image with the given lines drawn on."""
+    imageCopy = image.copy()
+
+    for rho, theta in lines:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*a)
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*a)
+
+        cv2.line(imageCopy, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    return imageCopy
