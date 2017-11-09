4f76a06a8b5a81fd16964cf984ef8ea357e2a098
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-09 16:49:11 +0100

    Filter out lines which are not close to horizontal or vertical

--- scancube.py
+++ scancube.py
@@ -2,12 +2,18 @@ import cv2
 import numpy as np
 
 
-def scan_cube(cubeImage, edgeImageFile=None, linesImageFile=None):
+def scan_cube(
+        cubeImage,
+        edgeImageFile=None,
+        linesImageFile=None,
+        orthogonalLinesImageFile=None):
     """Scan the given cube image and return the colours of the cube face.
 
     Returns None if cube face could not be scanned.
     Saves the intermediate edge image generated to the filename given.
     Saves the intermediate lines image generated to the filename given.
+    Saves the intermediate orthogonal lines image generated to the filename
+    given.
     """
     edgeImage = _detect_edges(cubeImage)
     if edgeImageFile is not None:
@@ -17,6 +23,11 @@ def scan_cube(cubeImage, edgeImageFile=None, linesImageFile=None):
     if linesImageFile is not None:
         linesImage = _draw_lines(cubeImage, lines)
         cv2.imwrite(linesImageFile, linesImage)
+
+    orthogonalLines = _horizontal_and_vertical_lines(lines)
+    if orthogonalLinesImageFile is not None:
+        orthogonalLinesImage = _draw_lines(cubeImage, orthogonalLines)
+        cv2.imwrite(orthogonalLinesImageFile, orthogonalLinesImage)
 
     return None
 
@@ -56,3 +67,23 @@ def _draw_lines(image, lines):
         cv2.line(imageCopy, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     return imageCopy
+
+
+def _is_horizontal(line):
+    """Returns True if line is within 1/36pi of horizontal."""
+    _, theta = line
+    return theta > np.pi*17/36 and theta < np.pi*19/36
+
+
+def _is_vertical(line):
+    """Returns True if line is within 1/36pi of vertical."""
+    _, theta = line
+    return theta > np.pi*35/36 and theta < np.pi/36
+
+
+def _horizontal_and_vertical_lines(lines):
+    """Returns only horizontal and vertical lines of the lines given."""
+    return [
+        line for line in lines
+        if _is_horizontal(line) or _is_vertical(line)
+    ]
